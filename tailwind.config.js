aec551c5da4211b1b834ba5082ec69b699853c3a
Mohibbur Rahman <mohibbur1994@gmail.com>
2024-11-15 11:19:03 +0600

    dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import flowbite from 'flowbite/plugin'; // Import the Flowbite plugin
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -9,6 +10,7 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './node_modules/flowbite/**/*.js', // Add Flowbite's components here
     ],
 
     darkMode: 'class', // Enable dark mode with the 'class' strategy
@@ -21,5 +23,5 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography, flowbite], // Add Flowbite to the plugins array
 };
